ac237b8cd9fb4df514e4bbfad27bcc9e74c6307b
Timo Früh <dev@tifrueh.ch>
2024-09-24 09:37:09 +0200

    Introduce the qls subcommand
    This subcommand can be used to print the questions and their
    corresponding files instead of the processed anki file.

--- ankidoc.sh
+++ ankidoc.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-help="Usage: ${0} <file> ..."
+help="Usage: ${0} [ qls ] <file> ..."
+
+qlist=0
+
+if [ "${1}" = "qls" ]; then
+    qlist=1
+    shift
+fi
 
 for file in $@; do
     if ! [ -f $file ]; then
@@ -38,13 +45,30 @@ function generate_card() {
     printf '"%s";"%s";"%s"' "${card_id}" "${front_html}" "${back_html}"
 }
 
-printf '%s' "${header}"
+function generate_all() {
 
-for file in $@; do
-    if [ ${file##*.} = "front" ] || [ ${file##*.} = "back" ]; then
-        printf '%s\n' "$(generate_card ${file})"
-    else
-        printf '%s\n' "${argv0}: not processing ${file}" 1>&2
-        continue
-    fi
-done
+    printf '%s' "${header}"
+
+    for file in $@; do
+        if [ ${file##*.} = "front" ] || [ ${file##*.} = "back" ]; then
+            printf '%s\n' "$(generate_card ${file})"
+        else
+            printf '%s\n' "${argv0}: not processing ${file}" 1>&2
+            continue
+        fi
+    done
+}
+
+function qlist() {
+    for file in $@; do
+        if [ ${file##*.} = "front" ]; then
+            printf '%s : %s\n' "${file}" "$(cat ${file})"
+        fi
+    done
+}
+
+if [ $qlist -eq 1 ]; then
+    qlist $@
+else
+    generate_all $@
+fi
